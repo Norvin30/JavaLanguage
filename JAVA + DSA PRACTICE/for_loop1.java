3542b991d2160650182ceafdc08de15596b086f4
Norvin30 <108176736+Norvin30@users.noreply.github.com>
2024-05-08 17:51:03 +0530

    Delete JAVA + DSA PRACTICE directory

--- JAVA + DSA PRACTICE/for_loop1.java
+++
@@ -1,8 +0,0 @@
-// Print Hello World 100 times
-public class for_loop1 {
-    public static void main(String[] args){
-        for ( int i = 0; i <= 100; i++){
-            System.out.println("Hello world");
-        }
-    }
-}
